cf422ce3f5724cf739489a30d9dc47f7e2a92c16
rozenberyl <rozenberyl@rozenberyldeMacBook-Pro.local>
2019-10-30 13:05:32 -0400

    point.cc

--- psi4/src/psi4/libfock/points.cc
+++ psi4/src/psi4/libfock/points.cc
@@ -786,6 +786,7 @@ void BasisFunctions::compute_functions(std::shared_ptr<BlockOPoints> block) {
             double* phi_yy_start = tmp_yyp + row_shift;
             double* phi_yz_start = tmp_yzp + row_shift;
             double* phi_zz_start = tmp_zzp + row_shift;
+
             gg_collocation_deriv2(L, npoints, xyz, 1, nprim, norm, alpha, center.data(), order, phi_start,
                                   phi_x_start, phi_y_start, phi_z_start, phi_xx_start, phi_xy_start, phi_xz_start,
                                   phi_yy_start, phi_yz_start, phi_zz_start);
