60feaeb18182ae247eb71b0f284ede7a68182b37
rozenberyl <rozenberyl@rozenberyldeMacBook-Pro.local>
2020-02-11 16:09:14 -0500

    fixed PSI_WT_TOCLEN() error

--- psi4/src/psi4/cctransort/sort_tei_rhf.cc
+++ psi4/src/psi4/cctransort/sort_tei_rhf.cc
@@ -34,13 +34,24 @@ namespace cctransort {
 
 void sort_tei_rhf(std::shared_ptr<PSIO> psio, int print) {
     dpdbuf4<double> K;
+    dpdbuf4<float> K_test;
 
     psio->open(PSIF_CC_AINTS, PSIO_OPEN_OLD);
     global_dpd_->buf4_init(&K, PSIF_LIBTRANS_DPD, 0, "ij", "kl", "i>=j+", "k>=l+", 0, "MO Ints (OO|OO)");
     global_dpd_->buf4_sort(&K, PSIF_CC_AINTS, prqs, "ij", "kl", "A <ij|kl>");
+
+    //Test the buf4_copy function
+    global_dpd_->buf4_close(&K);
+    global_dpd_->buf4_init(&K, PSIF_CC_AINTS, 0, "ij", "kl", 0, "A <ij|kl>");    
+    //global_dpd_->buf4_copy(&K, PSIF_CC_AINTS, "A <ij|kl> copy"); 
+   // global_dpd_->buf4_init(&K_test, PSIF_CC_AINTS, 0, "ij", "kl", 0, "A <ij|kl> copy");
+
     //Cast and make a copy of buf4 in single-precision
     //if ((precision == 1)||(precision == 2)) global_dpd_->buf4_cast_copy(&K, PSIF_CC_AINTS, "ij", "kl", "A <ij|kl> sp");
     global_dpd_->buf4_cast_copy_dtof(&K, PSIF_CC_AINTS,  "A <ij|kl> sp");
+    
+    // Test the cast copy function
+    //global_dpd_->buf4_init_sp(&K_test, PSIF_CC_AINTS, 0, "ij", "kl", 0, "A <ij|kl> sp");
 
     global_dpd_->buf4_close(&K);
     if (print > 6) {
@@ -55,7 +66,9 @@ void sort_tei_rhf(std::shared_ptr<PSIO> psio, int print) {
     global_dpd_->buf4_sort(&K, PSIF_CC_BINTS, prqs, "ab", "cd", "B <ab|cd>");
     //Cast and make a copy of buf4 in single-precision
     //if ((precision == 1)||(precision == 2)) global_dpd_->buf4_cast_copy(&K, PSIF_CC_AINTS, "ab", "cd", "B <ab|cd> sp");
-    global_dpd_->buf4_cast_copy_dtof(&K, PSIF_CC_AINTS, "B <ab|cd> sp");
+    global_dpd_->buf4_close(&K);
+    global_dpd_->buf4_init(&K, PSIF_CC_BINTS, 0, "ab", "cd", 0, "B <ab|cd>");
+    global_dpd_->buf4_cast_copy_dtof(&K, PSIF_CC_BINTS, "B <ab|cd> sp");
     global_dpd_->buf4_close(&K);
     if (print > 6) {
         global_dpd_->buf4_init(&K, PSIF_CC_BINTS, 0, "ab", "cd", 0, "B <ab|cd>");
@@ -69,6 +82,8 @@ void sort_tei_rhf(std::shared_ptr<PSIO> psio, int print) {
     global_dpd_->buf4_sort(&K, PSIF_CC_CINTS, prqs, "ia", "jb", "C <ia|jb>");
     //Cast and make a copy of buf4 in single-precision
     //if ((precision == 1)||(precision == 2)) global_dpd_->buf4_cast_copy(&K, PSIF_CC_AINTS, "ia", "jb", "C <ia|jb> sp");
+    global_dpd_->buf4_close(&K);
+    global_dpd_->buf4_init(&K, PSIF_CC_CINTS, 0, "ia", "jb", 0, "C <ia|jb>");
     global_dpd_->buf4_cast_copy_dtof(&K, PSIF_CC_CINTS, "C <ia|jb> sp");
 
     global_dpd_->buf4_close(&K);
@@ -84,6 +99,8 @@ void sort_tei_rhf(std::shared_ptr<PSIO> psio, int print) {
     global_dpd_->buf4_sort(&K, PSIF_CC_DINTS, prqs, "ij", "ab", "D <ij|ab>");
     //Cast and make a copy of buf4 in single-precision
     //if ((precision == 1)||(precision == 2)) global_dpd_->buf4_cast_copy(&K, PSIF_CC_AINTS, "jb", "ia", "D <jb|ia> sp");
+    global_dpd_->buf4_close(&K);
+    global_dpd_->buf4_init(&K, PSIF_CC_DINTS, 0, "ij", "ab", 0, "D <ij|ab>");
     global_dpd_->buf4_cast_copy_dtof(&K, PSIF_CC_DINTS, "D <ij|ab> sp");
     global_dpd_->buf4_close(&K);
     if (print > 6) {
@@ -98,6 +115,8 @@ void sort_tei_rhf(std::shared_ptr<PSIO> psio, int print) {
     global_dpd_->buf4_sort(&K, PSIF_CC_EINTS, sqrp, "ai", "jk", "E <ai|jk>");
     //Cast and make a copy of buf4 in single-precision
     //if ((precision == 1)||(precision == 2)) global_dpd_->buf4_cast_copy(&K, PSIF_CC_AINTS, "ai", "jk", "E <ai|jk> sp");
+    global_dpd_->buf4_close(&K);
+    global_dpd_->buf4_init(&K, PSIF_CC_EINTS, 0, "ai", "jk", 0, "E <ai|jk>");
     global_dpd_->buf4_cast_copy_dtof(&K, PSIF_CC_EINTS, "E <ai|jk> sp");
     global_dpd_->buf4_close(&K);
     if (print > 6) {
@@ -112,6 +131,8 @@ void sort_tei_rhf(std::shared_ptr<PSIO> psio, int print) {
     global_dpd_->buf4_sort(&K, PSIF_CC_FINTS, prqs, "ia", "bc", "F <ia|bc>");
     //Cast and make a copy of buf4 in single-precision
     //if ((precision == 1)||(precision == 2)) global_dpd_->buf4_cast_copy(&K, PSIF_CC_AINTS, "ia", "bc", "F <ia|bc> sp");
+    global_dpd_->buf4_close(&K);
+    global_dpd_->buf4_init(&K, PSIF_CC_FINTS, 0, "ia", "bc", 0, "F <ia|bc>");
     global_dpd_->buf4_cast_copy_dtof(&K, PSIF_CC_FINTS, "F <ia|bc> sp");
 
     global_dpd_->buf4_close(&K);
@@ -123,8 +144,9 @@ void sort_tei_rhf(std::shared_ptr<PSIO> psio, int print) {
     global_dpd_->buf4_init(&K, PSIF_CC_FINTS, 0, "ia", "bc", 0, "F <ia|bc>");
     global_dpd_->buf4_sort(&K, PSIF_CC_FINTS, qpsr, "ai", "bc", "F <ai|bc>");
     global_dpd_->buf4_close(&K);
-    global_dpd_->buf4_init(&K, PSIF_CC_FINTS, 0, "ia", "bc", 0, "F <ai|bc>");
+    global_dpd_->buf4_init(&K, PSIF_CC_FINTS, 0, "ai", "bc", 0, "F <ai|bc>");
     global_dpd_->buf4_cast_copy_dtof(&K, PSIF_CC_FINTS, "F <ai|bc> sp");
+    global_dpd_->buf4_close(&K);
     psio->close(PSIF_CC_FINTS, 1);
 }
 
